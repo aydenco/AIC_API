b6e0e35bcb615cd02ea1b6a79ac2d31cfff47e3c
Ayden Overson <ayden99@gmail.com>
2023-02-16 16:29:37 -0700

    working

--- main.js
+++ main.js
@@ -1,52 +1,25 @@
-// Declarations for our song values
 let art;
 let showInfo;
-
-// Spotify Client Creds
-// const clientId = '7c6c168ee3e74062ae7411dc4cb18b16';
-// const clientSecret = '9b73bbf5a3a248b68b21bcb2a2fa46ec';
-
-// const _getToken = async () => {
-//     const result = await fetch(`https://accounts.spotify.com/api/token`, {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/x-www-form-urlencoded',
-//             'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret)
-//         },
-//         body: 'grant_type=client_credentials'
-//     });
-
-//     // Access the data given to us by the fetch response (Promise)
-//     const data = await result.json();
-//     return data.access_token
-// }
 
 // Function to get art info when image figure is clicked
 /**
  * @param img_index
  * @param item_index
  * 
- * Function gets a art from spotify using the image index of our gallery. Then
- * it finds the correct item_index inside of the JSON response data from Spotify
- * which will produce a preview URL that will be used to play our selected art.
+ * Function gets info from AIC using the image index of our gallery. Then
+ * it finds the correct item_index inside of the JSON response data from AIC
+ * which will produce a preview URL that will be used to display our selected art's info.
  * 
  */
 
-async function clickedEvent(img_index, item_index){
-    // Get piece name
+async function clickedEvent(img_index, item_index) {
     let piece = document.getElementsByTagName('img')[img_index].attributes[2].value
 
-    // Get our token
-    // let token = await _getToken();
-
     let headers = new Headers([
-        ['Content-Type', 'application/json'],
-        ['Accept', 'application/json'],
-        ['AIC-User-Agent, coding_temple_homework (aydenoa99@gmail.com)']
-        // ['Authorization', `Bearer ${token}`]
+        ['Content-Type', 'application/json']
     ]);
 
-    let request = new Request(`https://api.artic.edu/api/v1/artworks/${piece}`,{
+    let request = new Request(`https://api.artic.edu/api/v1/artworks/${piece}`, {
         method: 'GET',
         headers: headers
     });
@@ -54,15 +27,10 @@ async function clickedEvent(img_index, item_index){
     let result = await fetch(request);
 
     let response = await result.json();
-
+    let test = document.getElementById("test")
+    test.innerHTML = ` ID: ${response.data.id}<br> Title: ${response.data.title}<br> Artist: ${response.data.artist_display}<br> Medium: ${response.data.medium_display}`
     console.log(response)
-    let art = response.pieces.items[item_index].preview_url
-
-    // Before we play a art, first check if showInfo is True, and if so, stop it
-    if (showInfo){
-        stopSnippet();
-    }
-    artSnippet(art);
+    // let art = response.pieces.items[item_index].preview_url
 }
 
 /**
@@ -75,60 +43,42 @@ async function clickedEvent(img_index, item_index){
  * Our function will produce arts from the clickeEvent based on the index of the image.
  */
 
-function getart(id, event){
-    switch(id){
-        case 'fig1': { // The Alchemist -Haarper
+function getArt(id, event) {
+    switch (id) {
+        case 'fig1': { // "Anacreon with the Infants Bacchus and Cupid"
             event.stopPropagation();
-            clickedEvent(0,0)
+            clickedEvent(0, 0)
             break;
         }
-        case 'fig2': { // BOONDOCK -NotNevi
+        case 'fig2': { // "Fantasy on the Death of Seneca"
             event.stopPropagation();
-            clickedEvent(1,0)
+            clickedEvent(1, 0)
             break;
         }
-        case 'fig3': { // Sympathy Coil -Shadient
+        case 'fig3': { // "The Dragon Devouring the Companions of Cadmus"
             event.stopPropagation();
-            clickedEvent(2,0)
+            clickedEvent(2, 0)
             break;
         }
-        case 'fig4': { // Tonight - Jker
+        case 'fig4': { // "Portrait Head of Emperor Hadrian"
             event.stopPropagation();
-            clickedEvent(3,0)
+            clickedEvent(3, 0)
             break;
         }
-        case 'fig5': { // Cult of the Lamp -Willro
+        case 'fig5': { // "Hercules and the Lernaean Hydra"
             event.stopPropagation();
-            clickedEvent(4,0)
+            clickedEvent(4, 0)
             break;
         }
-        case 'fig6': { // Limbo -Freddie Dredd
+        case 'fig6': { // "Aureus (Coin) Portraying Emperor Marcus Aurelius"
             event.stopPropagation();
-            clickedEvent(5,0)
+            clickedEvent(5, 0)
+            break;
+        }
+        case 'fig7': { // "Seneca"
+            event.stopPropagation();
+            clickedEvent(6, 0)
             break;
         }
     }
-}
-
-/**
- * @param url
- * 
- * url is the art preview url
- * 
- * function will return an audio clip given by the url
- */
-
-function artSnippet(url){
-    showInfo = new Audio(url);
-    return showInfo.play()
-}
-
-/**
- * NO PARAMS
- * 
- * Function returns an event to stop the art snippet
- */
-
-function  stopSnippet(){
-    return showInfo.pause();
 }
